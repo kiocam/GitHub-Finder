494e7e68aa2ee9b15ebca9efb503b0ddc9bdd2a7
Jason Cameron <jasoncameron246@gmail.com>
2020-02-12 00:04:20 -0500

    navbar setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import Navbar from './components/layouts/Navbar'
 import './App.css'
 
 class App extends React.Component {
   render() {
+    const name = 'john doe'
     return (
       <div className="App">
-        <h1>Hello world!!</h1>
+        <Navbar />
       </div>
     )
   }
